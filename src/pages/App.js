20cb55778f9b2a2463566d0a1278ea4c0da261af
Aquesha Addison <aquesha.addison@outlook.com>
2025-03-12 08:49:29 -0500

    Update and rename Index.js to App.js

---
+++ src/pages/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Certifications from "./pages/Certifications";
+import Dashboards from "./pages/Dashboards";
+import Writing from "./pages/Writing";
+import Coding from "./pages/Coding";
+import Projects from "./pages/Projects";
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/certifications" element={<Certifications />} />
+                <Route path="/dashboards" element={<Dashboards />} />
+                <Route path="/writing" element={<Writing />} />
+                <Route path="/coding" element={<Coding />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
+
